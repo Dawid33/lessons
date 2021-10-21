0367138e5d60623743da1ac2a41733ff95052196
Dawid Sobczak <dawidsobczak@fastmail.com>
2021-10-21 12:43:48 +0100

    First commit

---
+++ lesson_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+main() {                // Define the fuction main.
+    int x = 0;          // Create a variable 'x' with the number 0 stored inside it.
+    x = 30;             // Assign the value 30 to the variable
+    printf("%d", x);    // Print the value inside the variable to the screen  
+}
